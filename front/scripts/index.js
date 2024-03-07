2f5b2036fd207636ef77dd6b2b54b6e815e6f66e
unknown <jperdomo@iegabo.edu.co>
2024-03-06 20:05:26 -0500

    cambios con el ajax y bootstrap y algunas correciones de css

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,36 +1,35 @@
-///import tempData from "./tempData.js";
-// console.log(tempData);
+const cardContainer = document.getElementById("div-card");
 
-const newDesign = tempData.map((films) => {
-  //crear el contenedor de las tarjetas y los elementos de la misma
-  const card = document.getElementById("div-card");
-  const Ttitle = document.createElement("h3");
-  const Tyear = document.createElement("p");
-  const Tdirector = document.createElement("p");
-  const Tgender = document.createElement("p");
-  const Tflyer = document.createElement("img");
+$.get("https://students-api.2.us-1.fl0.io/movies", (films) => {
+  films.forEach((movie, index) => {
+    const cardFragment = document.createDocumentFragment();
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.id = "card" + index;
+    //crear el contenedor de las tarjetas y los elementos de la misma
+    const Ttitle = document.createElement("h3");
+    const Tyear = document.createElement("p");
+    const Tdirector = document.createElement("p");
+    const Tgender = document.createElement("p");
+    const Tflyer = document.createElement("img");
+    Tflyer.classList.add("card-img-top");
+    //conectar los elementos que debe traer del tempData
+    Ttitle.innerHTML = movie.title;
+    Tyear.innerHTML = movie.year;
+    Tdirector.innerHTML = movie.director;
+    Tgender.innerHTML = movie.genre;
+    Tflyer.src = movie.poster;
 
-  //conectar los elementos que debe traer del tempData
-  Ttitle.innerHTML = films.title;
-  Tyear.innerHTML = films.year;
-  Tdirector.innerHTML = films.director;
-  Tgender.innerHTML = films.genre;
-  Tflyer.src = films.poster;
+    //agregar los elementos en el div
+    cardFragment.appendChild(Ttitle);
+    cardFragment.appendChild(Tyear);
+    cardFragment.appendChild(Tdirector);
+    cardFragment.appendChild(Tgender);
+    cardFragment.appendChild(Tflyer);
 
-  //dar estilos css
-  Ttitle.classList.add("title-card");
-  Tyear.classList.add("year-card");
-  Tdirector.classList.add("director-card");
-  Tgender.classList.add("gender-card");
-  Tflyer.classList.add("flyer-card");
-
-  //agregar los elementos en el div
-  card.appendChild(Ttitle);
-  card.appendChild(Tyear);
-  card.appendChild(Tdirector);
-  card.appendChild(Tgender);
-  card.appendChild(Tflyer);
-  return card;
+    cardContainer.appendChild(card);
+    card.appendChild(cardFragment);
+    return cardContainer;
+  });
 });
-
-// console.log(newDesign);
+cardContainer.classList.add("contenido");
